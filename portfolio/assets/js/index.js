34c0cbdea179a4e15afa0267b52e303cadae1cf3
Kate Blazhko <kateugnicheva@gmail.com>
2022-01-28 17:53:17 +0300

    fix: correct modules

--- portfolio/assets/js/index.js
+++ portfolio/assets/js/index.js
@@ -1,41 +1,164 @@
-import * as mainFunction from  './functions.js';
-import * as localStorage from  './localStorage.js';
+import {addClassName, removeClassName, toggleClassName} from  './functions.js';
+import {setLocalStorage, getLocalStorage} from  './localStorage.js';
+import i18nObj from './translate.js';
 
+function scrollHeader() {
+  const header = document.querySelector('.header-container-inner');
+  if (window.scrollY > 0) {
+      addClassName('fixed', header);
+  } else {
+      removeClassName('fixed', header);
+  }
+}
+
+function toggleMenu() {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menuContainer = document.querySelector('.header-container-menu');
+  const body = document.querySelector('body');
+
+  menuToggle.onmousedown = function() {
+    return false;
+  };
+
+  toggleClassName('active', menuToggle, menuContainer);
+  toggleClassName('lock', body);
+}
+
+
+function closeMenu(event) {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const menuContainer = document.querySelector('.header-container-menu');
+  const body = document.querySelector('body');
+  if (event.target.classList.contains('nav-link')) {
+    removeClassName('active', menuToggle, menuContainer);
+    removeClassName('lock', body);
+  }
+}
+
+function changeImage(event) {
+  const portfolioImages = document.querySelectorAll('.portfolio-img');
+  if(event.target.classList.contains('portfolio-button')) {
+    let season = event.target.dataset.season;
+    portfolioImages.forEach((img, index) => img.src = `assets/img/${season}/${index + 1}.jpg`);
+  }
+}
+
+function changeBtn(event) {
+  const portfolioBtns = document.querySelectorAll('.portfolio-button');
+  if(event.target.classList.contains('portfolio-button')) {
+    portfolioBtns.forEach((btn) => btn.classList.remove('active'));
+    addClassName('active', event.target);
+  }
+}
+
+function cachedImages(season) {
+  for(let i = 1; i <= 6; i++) {
+    const img = new Image();
+    img.src = `assets/img/${season}/${i}.jpg`;
+  }
+}
+
+function changeLang(event) {
+  if (event.target.classList.contains('lang')) {
+    let lang = event.target.dataset.i18n;
+    getTranslate(lang);
+  }
+}
+
+function getTranslate(lang) {
+  const elementforTranslate = document.querySelectorAll('[data-i18n]');
+  elementforTranslate.forEach(element => {
+    let text = element.dataset.i18n;
+    element.textContent =  i18nObj[lang][text];
+  });
+  changeWidthToggle(lang);
+}
+
+function changeWidthToggle(lang) {
+  const langToggle = document.querySelector('.lang-toggle');
+  const headerMenu = document.querySelector('.header-container-menu');
+  if (lang === 'ru') {
+    addClassName('ru', langToggle, headerMenu);
+  } else {
+    removeClassName('ru', langToggle, headerMenu);
+  }
+}
+
+function changeTheme() {
+  toggleClassName('light', document.documentElement);
+}
+
+function changeSVG() {
+  const themeToggleUse = document.querySelector('.theme-use');
+  if (document.documentElement.classList.contains('light')) {
+    themeToggleUse.setAttributeNS('http://www.w3.org/1999/xlink', 'href', './assets/svg/sprite.svg#moon');
+  } else {
+    themeToggleUse.setAttributeNS('http://www.w3.org/1999/xlink', 'href', './assets/svg/sprite.svg#sun');
+  }
+}
+
+function createClickEffect(event) {
+  let element = event.target;
+  const x = event.clientX;
+  const y = event.clientY;
+
+  const buttonCoord = element.getBoundingClientRect();
+
+  const xInside = x - buttonCoord.x;
+  const yInside = y - buttonCoord.y;
+
+  const circle = document.createElement('span');
+  circle.classList.add('circle');
+  circle.style.top = yInside + 'px';
+  circle.style.left = xInside + 'px';
+
+  element.append(circle);
+
+  setTimeout(() => circle.remove(), 500);
+
+}
+
+window.onload = function () {
+  setTimeout(function () {
+      document.getElementsByTagName("body")[0].style.visibility = "visible";
+  }, 500);
+}
+//=============================================================================
 // local storage
-window.addEventListener('beforeunload', localStorage.setLocalStorage);
-window.addEventListener('load', localStorage.getLocalStorage);
+window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('load', getLocalStorage);
 
 // translated
 const langToggle = document.querySelector('.lang-toggle');
-langToggle.addEventListener('click', mainFunction.changeLang);
+langToggle.addEventListener('click', changeLang);
 
 // header scrolling
-window.addEventListener('scroll', mainFunction.scrollHeader);
+window.addEventListener('scroll', scrollHeader);
 
 // burger-menu
 const menuToggle = document.querySelector('.menu-toggle');
 const menu = document.querySelector('.nav');
-menuToggle.addEventListener('click', mainFunction.toggleMenu);
-menu.addEventListener('click', mainFunction.closeMenu);
+menuToggle.addEventListener('click', toggleMenu);
+menu.addEventListener('click', closeMenu);
 
 // change image for portfolio
 const portfolioForm = document.querySelector('.portfolio-form');
-portfolioForm.addEventListener('click', mainFunction.changeImage);
-portfolioForm.addEventListener('click', mainFunction.changeBtn);
+portfolioForm.addEventListener('click', changeImage);
+portfolioForm.addEventListener('click', changeBtn);
 
 // cached image
 const seasons = ['winter', 'spring', 'summer', 'autumn'];
-seasons.forEach(season =>mainFunction.cachedImages(season));
+seasons.forEach(season => cachedImages(season));
 
 // change theme
 const themeToggle = document.querySelector('.theme');
-themeToggle.addEventListener('click', mainFunction.changeTheme);
-themeToggle.addEventListener('click', mainFunction. changeSVG);
+themeToggle.addEventListener('click', changeTheme);
+themeToggle.addEventListener('click', changeSVG);
 
 // click effect
 const button = document.querySelectorAll('.button');
 button.forEach(element => {
-  element.addEventListener('click', mainFunction.createClickEffect)
+  element.addEventListener('click', createClickEffect)
 });
 
 console.log(`
