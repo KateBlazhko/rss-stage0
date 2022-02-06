eead3ac5f1e004ec7ccff661a20623bc091b5d9d
Kate Blazhko <kateugnicheva@gmail.com>
2022-02-06 23:33:13 +0300

    feat: add input attributes

--- image-galery/js/data.js
+++ image-galery/js/data.js
@@ -28,6 +28,7 @@ function createImage(data) {
 }
 
 async function getData() {
+  let url = `https://api.unsplash.com/search/photos?query=${value}&per_page=30&orientation=landscape&client_id=9339eeHreXZVOwRBse7FNOR9WXu-mRVnSM922YMjFd4`;
   const res = await fetch(url);
   const data = await res.json();
   showImg(data.results)
@@ -40,13 +41,12 @@ const inputSubcribe = () => {
 const checkEnter = (e) => {
   if (e.keyCode === 13) {
     value = searchInput.value;
-    url = `https://api.unsplash.com/search/photos?query=${value}&per_page=30&orientation=landscape&client_id=9339eeHreXZVOwRBse7FNOR9WXu-mRVnSM922YMjFd4`;
     getData()
   }
 }
 
-let value = '';
-let url = `https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=9339eeHreXZVOwRBse7FNOR9WXu-mRVnSM922YMjFd4`;
+let value = 'spring';
+
 const rend = new Renderer()
 
 export {getData, inputSubcribe}
